71bd68a56f6b78a33699cbaa4b0656d1eb709221
Matt Jonas <matt.jonas@gmail.com>
2015-08-25 13:04:08 -0700

    I did things

--- codeshipHelper.js
+++ codeshipHelper.js
@@ -1,49 +1,22 @@
 $(document).ready(function() {
-	var helper = new BudgetHelper();
-	setTimeout(helper.scrapeBudget, 1500);
+    var helper = new CodeshipHelper();
+    setTimeout(helper.setLinks, 1500);
 });
 
-var BudgetHelper = (function () {
-    var BudgetHelper = function () {
-        this.setupHandlers();
-        this.defineDomElements();
+var CodeshipHelper = (function() {
+    var CodeshipHelper = function() {
+        this.setLinks();
     };
 
-    BudgetHelper.prototype.setupHandlers = function () {
+    CodeshipHelper.prototype.setLinks = function() {
+
     };
 
-    BudgetHelper.prototype.defineDomElements = function () {
-    	this.spendingList = $('#spendingBudget-list-body')
-    	this.spendingBudgetEntries = this.spendingList.find('li');
-    };
-
-    BudgetHelper.prototype.scrapeBudget = function() {
-    	this.spendingList = $('#spendingBudget-list-body')
-    	this.spendingBudgetEntries = this.spendingList.find('li');
-
-    	//Wait until all ajax connections are closed.
-    	while($.active > 0){}
-
-    	var link = document.createElement('a');
-		link.textContent = 'Export data as CSV';
-		link.download = 'file.csv';
-		link.href = 'data:text/csv,'
-
-		var items = this.spendingBudgetEntries.map(function(idx, entry){
-			return {
-				category: $($(entry).find('a.title strong')[0]).html(),
-				actual: $($($(entry).find('div.progress strong.money')[0]).find('span')[1]).html().replace('$',''),
-				estimated: $($($(entry).find('div.progress strong.money')[1]).find('span')[1]).html().replace('$',''),
-			};
-		});
-
-    	var categories = items.each(function(idx, item){
-    		link.href += item.category + ',' + item.actual + ',' + item.estimated + '%0D%0A';
-    	});
-    	//right_col
-
-    	$('td.details-budgets').append(link);
-    };
-
-    return BudgetHelper;
+    return CodeshipHelper;
 })();
+
+$('span').filter(function() {
+    return $(this).text().trim().indexOf('http://bit.ly/') === 0;
+}).text(function () {
+    return $(this).text().replace("http://bit.ly/", "<a href='#'>Wat</a>"); 
+});
